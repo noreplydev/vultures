5ea71386d203af569a881ab3d64dcdc44df2656e
noreplydev <contact.cristiansanchez@gmail.com>
2025-03-15 19:53:12 +0100

    Add components folders to vultures app

--- packages/vultures/pages/index.tsx
+++ packages/vultures/pages/index.tsx
@@ -1,20 +1,5 @@
-export default function Home() {
-  return (
-    <div className="h-screen w-screen flex  flex-col font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col flex-1 items-center justify-center gap-5">
-        <h1
-          className="text-8xl font-bold"
-        >VULTURES</h1>
-        <button
-          className="border border-solid border-white px-4 py-2
-            text-xs hover:bg-white hover:text-black"
-        >
-          DASHBOARD
-        </button>
-      </main>
-      <footer className="flex w-full justify-center items-center p-5">
-        <code>CREAT0R</code>
-      </footer>
-    </div>
-  );
+import { Root } from "@/components/root";
+
+export default function RooPage() {
+  return <Root />
 }
