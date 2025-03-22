c188338ff095324df2ee97c350a11d211ea8e045
noreplydev <contact.cristiansanchez@gmail.com>
2025-03-23 00:11:32 +0100

    Update Input vultui component

--- packages/vultures/components/vultui/Input.tsx
+++ packages/vultures/components/vultui/Input.tsx
@@ -1,6 +1,9 @@
-import { c, colors, configs } from "@/theme"
+import { c, colors } from "@/theme";
+import React, { InputHTMLAttributes } from 'react';
 
-export const Input = ({ style, placeholder, type }: any) => {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
+
+export const Input: React.FC<InputProps> = ({ style, placeholder, type, onChange, value }) => {
   return <input
     style={{
       backgroundColor: colors.backgroundHighlight,
@@ -9,5 +12,5 @@ export const Input = ({ style, placeholder, type }: any) => {
       paddingBlock: c.sm.paddingBlock,
       fontSize: c.sm.fontSize,
       ...style
-    }} placeholder={placeholder} type={type} />
+    }} placeholder={placeholder} type={type} onChange={onChange} value={value} />
 }
