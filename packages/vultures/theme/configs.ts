69987a0cbadc72de61abde4f2dd4e6dac3e0fcce
noreplydev <contact.cristiansanchez@gmail.com>
2025-03-22 23:25:59 +0100

    Update theme configs

--- packages/vultures/theme/configs.ts
+++ packages/vultures/theme/configs.ts
@@ -2,16 +2,23 @@ export const configs = {
     sm: {
         paddingInline: "14px",
         paddingBlock: "8px",
-        gap: "3px",
+        gap: "5px",
+        fontSize: "17px",
+        borderRadius: "5px",
     },
     md: {
         paddingInline: "18px",
-        paddingBlock: "15px",
-        gap: "6px",
+        paddingBlock: "16px",
+        gap: "10px",
+        fontSize: "23px",
+        borderRadius: "8px",
     },
     lg: {
         paddingInline: "20px",
-        paddingBlock: "16px",
-        gap: "10px",
+        paddingBlock: "18px",
+        gap: "15px",
+        fontSize: "30px",
+        borderRadius: "12px",
     },
 };
+export const c = configs
