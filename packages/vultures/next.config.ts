4ef6dfc7f308b743aed1bf021ad07d93bc4cc7cb
noreplydev <contact.cristiansanchez@gmail.com>
2025-03-22 21:55:34 +0100

    Fix envars loading on nextjs

--- packages/vultures/next.config.ts
+++ packages/vultures/next.config.ts
@@ -1,9 +1,9 @@
-import { loadEnvConfig } from "@next/env";
+import dotenv from 'dotenv'
 import type { NextConfig } from "next";
 import path from "path";
 
-const envPath = path.join(__dirname, "../..")
-loadEnvConfig(envPath)
+const envPath = path.join(__dirname, "..", "..", ".env")
+dotenv.config({ path: envPath });
 
 const nextConfig: NextConfig = {
     /* config options here */
