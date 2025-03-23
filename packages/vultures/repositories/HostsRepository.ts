ce0a8c54dd471aa0c7cd09e9a8c2120d45488421
noreplydev <contact.cristiansanchez@gmail.com>
2025-03-23 21:42:36 +0100

    Create hosts as verified by default

--- packages/vultures/repositories/HostsRepository.ts
+++ packages/vultures/repositories/HostsRepository.ts
@@ -23,7 +23,7 @@ export class HostsRepository {
             const result: any = await fetch(apiPrefix + "/hosts", {
                 method: "POST",
                 headers: { "Content-type": "application/json; charset=UTF-8" },
-                body: JSON.stringify({ hostname, createdAt: Date.now() })
+                body: JSON.stringify({ hostname, createdAt: Date.now(), verified: true })
             })
 
             if (result["isError"]) {
