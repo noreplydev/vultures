b1b460d38653dfd395919ae029a6a6c3853d68da
noreplydev <contact.cristiansanchez@gmail.com>
2025-04-15 00:09:29 +0200

    Add proxy app to run api and vultures dashboard within a single entrypoint

---
+++ apps/proxy/index.js
@@ -0,0 +1,31 @@
+import { config as envConfig } from 'dotenv'
+import path from 'path'
+import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+envConfig({
+  path: path.join(__dirname, "..", "..", ".env")
+})
+
+const app = express();
+
+app.use('/api', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '',
+  },
+}));
+
+app.use('/', createProxyMiddleware({
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+}));
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Servidor proxy corriendo en el puerto ${PORT}`);
+});
